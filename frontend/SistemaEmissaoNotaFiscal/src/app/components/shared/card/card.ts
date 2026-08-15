991f0e41f9d92b6ec3094e42a6952a3b249d388e
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-08-15 10:22:34 -0300

    feat: Card component

---
+++ frontend/SistemaEmissaoNotaFiscal/src/app/components/shared/card/card.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css',
+})
+export class Card {
+  title = input.required<string>();
+  hint = input<string>();
+}
